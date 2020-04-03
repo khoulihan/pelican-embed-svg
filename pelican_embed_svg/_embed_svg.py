a26671ffdc141610ceb3ce2d94ae039d46b1677f
Kevin Houlihan <kevin@crimsoncookie.com>
2020-04-03 14:01:19 +0200

    Added fill settings Fixed various problems

--- pelican_embed_svg/_embed_svg.py
+++ pelican_embed_svg/_embed_svg.py
@@ -17,7 +17,7 @@ def _find_svg(available, name):
             return svg
 
 
-def _replace_icon(icon, name, classes, available):
+def _replace_icon(icon, name, classes, available, set_fill):
     modified = False
     svg = _find_svg(available, name)
     svg_element = None
@@ -29,9 +29,8 @@ def _replace_icon(icon, name, classes, available):
         # with Javascript. Unclear what the purpose of theese classes are
         # at this point.
         #svg_element.svg['class'].extend(['svg-inline--fa', 'fa-w-16'])
-        # Is this generally applicable? It allows the path fills to be effected
-        # by CSS color attributes.
-        svg_element.path['fill'] = 'currentColor'
+        if set_fill:
+            svg_element.path['fill'] = 'currentColor'
         modified = True
         icon.replace_with(svg_element)
     else:
@@ -39,34 +38,34 @@ def _replace_icon(icon, name, classes, available):
     return modified
 
 
-def _handle_icons(soup, icons, custom, fa_path):
+def _handle_icons(soup, icons, custom, fa_path, set_fill):
     modified = False
 
     def fa(style):
         name = classes[1][3:]
         fa_specific = fa_path / 'svgs' / style
         fa_available = sorted(fa_specific.glob('**/*.svg'))
-        return _replace_icon(icon, name, classes, fa_available)
+        return _replace_icon(icon, name, classes, fa_available, set_fill)
 
     for icon in icons:
         classes = icon['class']
         if 'pi' in classes:
-            modified = modified or _replace_icon(icon, classes[1], classes, custom)
+            modified = _replace_icon(icon, classes[1], classes, custom, set_fill) or modified
         elif 'fa' in classes:
             # TODO: This could also be ForkAwesome - need to look into how to support that
-            modified = modified or fa('solid')
+            modified = fa('solid') or modified
         elif 'fas' in classes:
-            modified = modified or fa('solid')
+            modified = fa('solid') or modified
         elif 'far' in classes:
-            modified = modified or fa('regular')
+            modified = fa('regular') or modified
         elif 'fal' in classes:
             # Don't know if this is the correct path!
-            modified = modified or fa('light')
+            modified = fa('light') or modified
         elif 'fad' in classes:
             # Don't know if this is the correct path!
-            modified = modified or fa('duotone')
+            modified = fa('duotone') or modified
         elif 'fab' in classes:
-            modified = modified or fa('brands')
+            modified = fa('brands') or modified
         elif classes[0].startswith('fi-'):
             # TODO: Friconix
             pass
@@ -74,10 +73,11 @@ def _handle_icons(soup, icons, custom, fa_path):
     return modified
 
 
-def _handle_imgs(soup, imgs, output_path, root_url):
+def _handle_imgs(soup, imgs, output_path, root_url, set_fill):
     modified = False
 
     for img in imgs:
+        print (img)
         src = img['src']
         if src.endswith('.svg'):
             if root_url and src.startswith(root_url):
@@ -88,9 +88,8 @@ def _handle_imgs(soup, imgs, output_path, root_url):
             if svg_element:
                 classes = img.get('class', None)
                 svg_element.svg['class'] = classes
-                # Extra unsure about setting this fill attribute here
-                # Maybe should make it a setting
-                svg_element.path['fill'] = 'currentColor'
+                if set_fill:
+                    svg_element.path['fill'] = 'currentColor'
                 img.replace_with(svg_element)
                 modified = True
 
@@ -101,9 +100,11 @@ def _finalized(p):
     output_path = Path(p.output_path)
     root_url = p.settings.get('SITEURL', '')
     process_img = p.settings.get('PES_EMBED_IMG_TAGS', True)
+    set_img_fill = p.settings.get('PES_SET_IMG_FILL', False)
+    set_icon_fill = p.settings.get('PES_SET_ICON_FILL', True)
     # Default is to search the entire output directory, though there is
     # actually no need for the images to be included in the output
-    svg_path = Path(p.settings.get('PES_SVG_PATH', p.output_path))
+    svg_path = Path(p.settings.get('PES_SVG_ICON_PATH', p.output_path))
     # The default here is a 'font-awesome' directory in the project root.
     fa_path = Path(p.settings.get('PES_FONT_AWESOME_PATH', Path('font-awesome')))
     svg_available = sorted(svg_path.glob('**/*.svg'))
@@ -114,10 +115,10 @@ def _finalized(p):
 
         modified = False
         icons = soup.find_all('i')
-        modified = modified or _handle_icons(soup, icons, svg_available, fa_path)
+        modified = _handle_icons(soup, icons, svg_available, fa_path, set_icon_fill) or modified
         if process_img:
             imgs = soup.find_all('img')
-            modified = modified or _handle_imgs(soup, imgs, output_path, root_url)
+            modified = _handle_imgs(soup, imgs, output_path, root_url, set_img_fill) or modified
 
         if modified:
             with open(f, 'w') as output:
